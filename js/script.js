c5cb819600da96584ac970e2135b8975e15e546f
Justin <justinereifanwijaya@gmail.com>
2023-12-23 11:47:05 +0700

    revisi web bryan ke5

--- js/script.js
+++ js/script.js
@@ -38,3 +38,37 @@ document.addEventListener("DOMContentLoaded", () => {
       content.style.display = "block";
   }
 });
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+const logo = document.querySelector('#logo');
+const iconlogo = document.querySelector('#iconlogo');
+
+
+
+darkToggle.addEventListener('click', function(){
+  if(darkToggle.checked){
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+    logo.src = '../assets/logo/logowhite.png';
+    iconlogo.src = '../assets/logo/iconlogo-white.png';
+    
+  }else{
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+    logo.src = '../assets/logo/logoblack.png';
+    iconlogo.src = '../assets/logo/iconlogo.png';
+  }
+})
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+  logo.src = '../assets/logo/logowhite.png';
+  iconlogo.src = '../assets/logo/iconlogo-white.png';
+} else {
+  darkToggle.checked = false;
+  logo.src = '../assets/logo/logoblack.png';
+  iconlogo.src = '../assets/logo/iconlogo.png';
+}
